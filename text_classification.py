73a8c178d4c17a3a62775977318e34866bf1a57d
Onaga Takaaki <tonaga@kanolab.net>
2022-06-20 16:16:01 +0900

    add

--- text_classification.py
+++ text_classification.py
@@ -16,3 +16,41 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 MODEL_NAME = 'cl-tohoku/bert-large-japanese'
 # いつもの cl-tohoku/bert-base-japanese-whole-word-masking
 
+# データを前処理
+category_list = [
+    'dokujo-tsushin',
+    'it-life-hack',
+    'kaden-channel',
+    'livedoor-homme',
+    'mobie-enter',
+    'peachy',
+    'smax',
+    'sports-watch',
+    'topic-news'
+]
+
+# トークナイザ
+tokenizer = BertJapaneseTokenizer.from_pretrained(MODEL_NAME)
+
+# データの形式を整える
+cnt = 0
+max_length = 512
+dataset_for_loader = []
+for label, category in enumerate(tqdm(category_list)):
+    for file_name in glob.glob(f'./data/text/{category}/{category}*'):
+        file = open(file_name,'r')
+        lines = file.readline().splitlines()
+        text = '\n'.join(lines[3:])
+        encoding = tokenizer(
+            text,
+            max_length = max_length,
+            padding = 'max_length',
+            truncation = True
+        )
+        encoding['labels'] = label
+        for k,v in encoding.items():
+            encoding = {k : torch.tensor(v)}
+        dataset_for_loader.append(encoding)
+
+    if cnt == 0:
+        print(dataset_for_loader[0:3])
