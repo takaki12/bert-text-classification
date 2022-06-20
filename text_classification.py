708ffc6f95392e8bf561a8d7ad8c0eeb58738b8c
Onaga Takaaki <tonaga@kanolab.net>
2022-06-20 15:40:28 +0900

    initial commit

--- text_classification.py
+++ text_classification.py
@@ -7,5 +7,12 @@ from torch.utils.data import DataLoader
 from transformers import BertJapaneseTokenizer, BertForSequenceClassification
 import pytorch_lightning as pl
 
+# GPU設定
+import os
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
 # 日本語学習モデル
 MODEL_NAME = 'cl-tohoku/bert-large-japanese'
+# いつもの cl-tohoku/bert-base-japanese-whole-word-masking
+
